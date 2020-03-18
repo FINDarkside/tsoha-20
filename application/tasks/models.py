ea08387e98f3aa0a68793d3c35d6f07f755d967b
Jesse Niininen <findarkside@gmail.com>
2020-03-18 17:36:42 +0200

    Init project

---
+++ application/tasks/models.py
@@ -0,0 +1,15 @@
+from application import db
+
+class FeatureRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    onupdate=db.func.current_timestamp())
+    user_id = db.Column(db.Integer, nullable=True)
+    title = db.Column(db.String(144), nullable=False)
+    description = db.Column(db.String(144), nullable=False)
+
+    def __init__(self, title, description, userId):
+        self.title = title
+        self.description = description
+        self.user_id = userId
