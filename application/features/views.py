d7264dcd59e542e725c860e6ee2bbb31c75de799
Jesse Niininen <findarkside@gmail.com>
2020-03-19 18:07:44 +0200

    Implement editing features

--- application/features/views.py
+++ application/features/views.py
@@ -6,10 +6,22 @@ from application.features.models import Feature
 def features_index():
     return render_template("features/list.html", features = Feature.query.all())
 
-
-@app.route("/features/new/")
+@app.route("/features/new/", methods=["GET"])
 def features_form():
     return render_template("features/new.html")
+
+@app.route("/features/<feature_id>/edit", methods=["GET"])
+def feature_requests_form(feature_id):
+    feature = Feature.query.get(feature_id)
+    return render_template("features/edit.html", feature = feature)
+
+@app.route("/features/<feature_id>/edit", methods=["POST"])
+def features_edit(feature_id):
+    f = Feature.query.get(feature_id)
+    f.title = request.form.get("title")
+    f.description = request.form.get("description")
+    db.session().commit()
+    return redirect(url_for("features_index"))
 
 @app.route("/features/", methods=["POST"])
 def features_create():
