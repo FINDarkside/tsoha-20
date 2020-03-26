8441b4330dc84457b43ca2fe48df59b9e86e8939
Jesse Niininen <findarkside@gmail.com>
2020-03-26 19:09:55 +0200

    Validate new features and show error messages

--- application/features/forms.py
+++ application/features/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, validators
 from wtforms.widgets import TextArea
 
 class FeatureForm(FlaskForm):
-    title = StringField("Title")
+    title = StringField("Title", [validators.Length(min=6)])
     description = StringField("Description", widget=TextArea())
  
     class Meta:
--- application/features/views.py
+++ application/features/views.py
@@ -3,18 +3,22 @@ from flask import redirect, render_template, request, url_for
 from application.features.models import Feature
 from application.features.forms import FeatureForm
 
+
 @app.route("/features/", methods=["GET"])
 def features_index():
-    return render_template("features/list.html", features = Feature.query.all())
+    return render_template("features/list.html", features=Feature.query.all())
+
 
 @app.route("/features/new/", methods=["GET"])
 def features_form():
-    return render_template("features/new.html", form = FeatureForm())
+    return render_template("features/new.html", form=FeatureForm())
+
 
 @app.route("/features/<feature_id>/edit", methods=["GET"])
 def feature_requests_form(feature_id):
     feature = Feature.query.get(feature_id)
-    return render_template("features/edit.html", feature = feature)
+    return render_template("features/edit.html", feature=feature)
+
 
 @app.route("/features/<feature_id>/edit", methods=["POST"])
 def features_edit(feature_id):
@@ -26,12 +30,16 @@ def features_edit(feature_id):
     db.session().commit()
     return redirect(url_for("features_index"))
 
+
 @app.route("/features/", methods=["POST"])
 def features_create():
     form = FeatureForm(request.form)
+    if not form.validate():
+        return render_template("features/new.html", form=form)
+
     feature = Feature(form.title.data,
-                       form.description.data,
-                       None)
+                      form.description.data,
+                      None)
 
     db.session().add(feature)
     db.session().commit()
