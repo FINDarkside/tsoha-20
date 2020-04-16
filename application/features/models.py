972b71521e8f626199614a6988aa6738995341b7
Jesse Niininen <findarkside@gmail.com>
2020-04-16 21:34:23 +0300

    Fix n+1 problem with like count

--- application/features/models.py
+++ application/features/models.py
@@ -1,7 +1,20 @@
 from application import db
 from application.models import Base
-from sqlalchemy import func
+from sqlalchemy import func, select
+from sqlalchemy.orm import column_property
+from sqlalchemy.sql import and_
 from flask_login import current_user
+
+
+class Like(Base):
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'account.id'), nullable=True)
+    feature_id = db.Column(db.Integer, db.ForeignKey(
+        'feature.id'), nullable=False)
+
+    def __init__(self, feature_id, user_id):
+        self.feature_id = feature_id
+        self.user_id = user_id
 
 
 class Feature(Base):
@@ -15,20 +28,11 @@ class Feature(Base):
         self.user_id = userId
 
     @property
-    def like_count(self):
-        return db.session.query(Feature.id).outerjoin(Like).filter_by(feature_id=self.id).count()
-
-    @property
     def authorized_to_modify(self):
         return current_user.is_admin or current_user.id == self.user_id
 
 
-class Like(Base):
-    user_id = db.Column(db.Integer, db.ForeignKey(
-        'account.id'), nullable=True)
-    feature_id = db.Column(db.Integer, db.ForeignKey(
-        'feature.id'), nullable=False)
-
-    def __init__(self, feature_id, user_id):
-        self.feature_id = feature_id
-        self.user_id = user_id
+Feature.like_count = column_property(
+    select([func.count(Like.id)]).
+    where(Like.feature_id == Feature.id)
+)
