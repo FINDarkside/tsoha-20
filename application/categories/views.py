849da0986aeaccd1e6912d8790e4705ddb425acd
Jesse Niininen <findarkside@gmail.com>
2020-04-30 02:39:39 +0300

    Implement ability to create new categories

--- application/categories/views.py
+++ application/categories/views.py
@@ -36,3 +36,24 @@ def categories_edit(category_id):
     db.session().commit()
 
     return render_template("categories/edit.html", categories=categories)
+
+
+@app.route("/categories/new", methods=["POST"])
+@login_required
+def categories_create():
+    if not current_user.is_admin:
+        return render_template("error.html", error="Unauthorized")
+
+    categories = FeatureCategory.get_all()
+
+    name = request.form.get('name')
+    if(not isinstance(name, str) or len(name) < 3):
+        return render_template("categories/edit.html", categories=categories, error="Category name must be at least 3 characters long")
+    if(len(name) > 20):
+        return render_template("categories/edit.html", categories=categories, error="Category name must be less than 21 characters long")
+
+    category = FeatureCategory(name)
+    db.session().add(category)
+    db.session().commit()
+    
+    return redirect(url_for("categories_edit_form"))
