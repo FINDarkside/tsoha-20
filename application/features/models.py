17b9fb062018aece23ba762093ed6e83296cac45
Jesse Niininen <findarkside@gmail.com>
2020-04-29 22:58:39 +0300

    Add feature category model

--- application/features/models.py
+++ application/features/models.py
@@ -1,6 +1,6 @@
 from application import db
 from application.models import Base
-from sqlalchemy import func, select
+from sqlalchemy import func, select, event
 from sqlalchemy.orm import column_property
 from sqlalchemy.sql import and_
 from flask_login import current_user
@@ -8,7 +8,7 @@ from flask_login import current_user
 
 class Like(Base):
     user_id = db.Column(db.Integer, db.ForeignKey(
-        'account.id'), nullable=True)
+        'account.id'), nullable=False)
     feature_id = db.Column(db.Integer, db.ForeignKey(
         'feature.id'), nullable=False)
 
@@ -17,15 +17,25 @@ class Like(Base):
         self.user_id = user_id
 
 
+class FeatureCategory(Base):
+    name = db.Column(db.String, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+
 class Feature(Base):
     user_id = db.Column(db.Integer, nullable=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(FeatureCategory.id))
+    category = db.relationship("FeatureCategory", foreign_keys=[category_id], lazy='joined')
 
     def __init__(self, title, description, userId):
         self.title = title
         self.description = description
         self.user_id = userId
+        self.category_id = 1
 
     @property
     def authorized_to_modify(self):
@@ -33,7 +43,8 @@ class Feature(Base):
 
     @property
     def current_user_liked(self):
-        count = db.session.query(Like).filter_by(user_id=current_user.id, feature_id=self.id).count()
+        count = db.session.query(Like).filter_by(
+            user_id=current_user.id, feature_id=self.id).count()
         return count > 0
 
 
@@ -41,3 +52,13 @@ Feature.like_count = column_property(
     select([func.count(Like.id)]).
     where(Like.feature_id == Feature.id)
 )
+
+
+def init_categories(*args, **kwargs):
+    # FIXME: Read password from env variable or remove this completely!
+    db.session.add(FeatureCategory("Open"))
+    db.session.add(FeatureCategory("Done"))
+    db.session.commit()
+
+
+event.listen(FeatureCategory.__table__, 'after_create', init_categories)
